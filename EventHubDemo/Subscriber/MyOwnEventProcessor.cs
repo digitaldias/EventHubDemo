94efd6a3480279e745c2ce7e1d299eebc7a8b9aa
Pedro G. Dias <pedro@digitaldias.com>
2015-11-06 07:39:21 +0100

    Added a Subscriber project and implemented IEventProcessor

---
+++ EventHubDemo/Subscriber/MyOwnEventProcessor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.ServiceBus.Messaging;
+using Domain;
+
+namespace Subscriber
+{
+    public class MyOwnEventProcessor : IEventProcessor
+    {
+        public async Task CloseAsync(PartitionContext context, CloseReason reason)
+        {
+           if(reason == CloseReason.Shutdown)
+            {
+                await context.CheckpointAsync();
+            }
+        }
+
+        public Task OpenAsync(PartitionContext context)
+        {
+            Console.WriteLine("Context Lease Opened on Partition " + context.Lease.PartitionId + " owned by " + context.Lease.Owner);
+            return Task.FromResult<object>(null);
+        }
+
+        public async Task ProcessEventsAsync(PartitionContext context, IEnumerable<EventData> messages)
+        {
+            foreach(var message in messages)
+            {                
+                var importantMeasure = ImportantMeasure.FromByteArray(message.GetBytes());
+                Console.WriteLine($"[Partition {context.Lease.PartitionId}] {DateTime.Now.ToString("HH:mm:ss")}: {importantMeasure.ImportantValue.ToString("0.00")}");
+            }            
+            await context.CheckpointAsync();
+        }
+    }
+}
---
+++ EventHubDemo/Subscriber/Program.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.ServiceBus.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Subscriber
+{
+    class Program
+    {
+        private static string _eventHubConnectionString = "Endpoint=sb://bigdatahub.servicebus.windows.net/;SharedAccessKeyName=processor;SharedAccessKey=DkNixCV7QUfwAsL1STbUnov6LBEEOj5zGQmJJ+MLuwE=";
+        private static string _eventHubPath = "bigdataeventhub";
+        private static string _storageConnectionString = "DefaultEndpointsProtocol=https;AccountName=lotsofdata;AccountKey=QoKJgHZGujg4t9hU1QOsV1OyYRT9mlqpvdkm48dCECQ/EHKUsrXpof/g4DLdCLJLMgyrYcHkMCjE/QoNiuq+lw==";
+        private static string _consumerGroupName = "bigdataconsumergroup";
+
+        private static EventProcessorHost _host;
+
+        static void Main(string[] args)
+        {
+            InitializeEventProcessorHost();
+
+            _host.RegisterEventProcessorAsync<MyOwnEventProcessor>().Wait();
+
+            Console.WriteLine("Press any key to end the program");
+            Console.ReadKey();
+        }
+
+        private static void InitializeEventProcessorHost()
+        {
+            Console.WriteLine("Initializing EventProcessor Host");
+
+            string hostName = CreateUniqueHostname();
+
+            try
+            {
+                _host = new EventProcessorHost(hostName, _eventHubPath, _consumerGroupName, _eventHubConnectionString, _storageConnectionString);
+                Console.WriteLine($"{hostName} created successfully.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Host creation failed: {ex.Message}\n\n{ex.StackTrace.ToString()}");
+                throw;
+            }
+        }
+
+        private static string CreateUniqueHostname()
+        {
+            return "HOST_" + Guid.NewGuid().ToString().Substring(0, 8);
+        }
+    }
+}
